57fe616d6ca8c830ab74b2aa5f38a287b5ee1ae8
mayurthole12 <66745938+mayurthole12@users.noreply.github.com>
2022-07-23 20:21:17 +0530

    Update smoke-google.sh

--- smoke-google.sh
+++ smoke-google.sh
@@ -2,6 +2,6 @@
 
 . smoke.sh
 
-smoke_url_ok "http://google.com/"
+smoke_url_ok "http://ec2co-ecsel-1cehtj2b7vsip-689193003.us-east-1.elb.amazonaws.com/"
     smoke_assert_body "search"
 smoke_report
